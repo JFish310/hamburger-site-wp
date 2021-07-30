72c1b2847de521bf510e779d479572e9643e8fce
mito <raimiti8@gmail.com>
2021-07-31 02:33:47 +0900

    リファクタリング終了

--- js/menu.js
+++ js/menu.js
@@ -1,13 +1,9 @@
 /* .c-button--js（Menuボタンと×ボタン）をクリックすると */
 $('.c-button--js').on('click',function(){
-
     // .l-sidebarにis-openクラスを付与
     // $('bady , .l-sidebar').toggleClass('is-open');
     $('body').toggleClass('is-open');
     $('.l-sidebar').toggleClass('is-open');
-
-    // // bodyにnoscrollクラスを付与(スクロールを固定)
-    // $('body').toggleClass('noscroll');
 
     // // .l-sidebarが0.5秒でフェードイン(メニューのフェードイン)
     // $('.l-sidebar.is-open').fadeToggle(500);
