710910bd69002cdaaa722315028be21b4ab0ac79
mito <raimiti8@gmail.com>
2021-09-02 01:59:56 +0900

    不要ソース削除

--- js/menu.js
+++ js/menu.js
@@ -1,10 +1,6 @@
 /* .c-button--js（Menuボタンと×ボタン）をクリックすると */
 $('.c-button--js').on('click',function(){
     // .l-sidebarにis-openクラスを付与
-    // $('bady , .l-sidebar').toggleClass('is-open');
     $('body').toggleClass('is-open');
     $('.l-sidebar').toggleClass('is-open');
-
-    // // .l-sidebarが0.5秒でフェードイン(メニューのフェードイン)
-    // $('.l-sidebar.is-open').fadeToggle(500);
 });
