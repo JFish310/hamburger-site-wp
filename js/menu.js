4dfa41b7a8f5699a9c2363023a0cfb30f2a11b8e
mito <raimiti8@gmail.com>
2022-12-14 12:11:43 +0900

    #01 スペース4をタブに修正

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,6 @@
 /* .c-button--js（Menuボタンと×ボタン）をクリックすると */
 $('.c-button--js').on('click',function(){
-    // .l-sidebarにis-openクラスを付与
-    $('body').toggleClass('is-open');
-    $('.l-sidebar').toggleClass('is-open');
+	// .l-sidebarにis-openクラスを付与
+	$('body').toggleClass('is-open');
+	$('.l-sidebar').toggleClass('is-open');
 });
