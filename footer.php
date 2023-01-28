86d152d367579c59aa97b45262b942d3a5774f0d
mito <raimiti8@gmail.com>
2023-01-28 21:39:19 +0900

    page.php（固定ページ）タイトルの取得を中心としたリファクタリング

--- footer.php
+++ footer.php
@@ -1,10 +1,14 @@
 	<footer class="l-footer">
-		<p>ショップ情報｜ヒストリー</p>
+		<!-- <p>ショップ情報｜ヒストリー</p> -->
+		<p>
+			<a href = "/information/">ショップ情報</a>｜<a href = "/history/">ヒストリー</a>
+		</p>
 		<p>
 			<small>Copyright: RaiseTech</small>
 			<!-- Copyright: RaiseTech -->
 		</p>
 	</footer>
 	<script src="/js/menu.js"></script>
+	<?php wp_footer(); ?>
 </body>
 </html>
--- functions.php
+++ functions.php
@@ -38,17 +38,17 @@
 
 	// ウィジェット用
 	// function hamburgersite_widgets_init() {
-	//     register_sidebar (
-	//         array(
-	//             'name'          => 'カテゴリーウィジェット',
-	//             'id'            => 'category_widget',
-	//             'description'   => 'カテゴリー用ウィジェットです',
-	//             'before_widget' => '<div id="%1$s" class="widget %2$s">',
-	//             'after_widget'  => '</div>',
-	//             'before_title'  => '<h2><i class="fa fa-folder-open" aria-hidden="true"></i>',
-	//             'after_title'   => "</h2>\n",
-	//         )
-	//     );
+	// 	register_sidebar (
+	// 		array(
+	// 			'name'          => 'カテゴリーウィジェット',
+	// 			'id'            => 'category_widget',
+	// 			'description'   => 'カテゴリー用ウィジェットです',
+	// 			'before_widget' => '<div id="%1$s" class="widget %2$s">',
+	// 			'after_widget'  => '</div>',
+	// 			'before_title'  => '<h2><i class="fa fa-folder-open" aria-hidden="true"></i>',
+	// 			'after_title'   => "</h2>\n",
+	// 		)
+	// 	);
 	// }
 	// add_action( 'widgets_init', 'hamburgersite_widgets_init' );
 
