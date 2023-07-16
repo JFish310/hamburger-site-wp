f422bb55a11d16185f877e6713ce8936269f0212
mito <raimiti8@gmail.com>
2023-07-16 20:06:36 +0900

    メニュが出たままウインドウサイズを広げたときの処理、グレーアウト部分をクリックしたとき閉じる処理追加

--- js/menu.js
+++ js/menu.js
@@ -4,48 +4,29 @@
 /* jQuery(document).ready(function($) {	 は						*/
 /* $(function($){	になる。									*/
 
-// jQuery(function($){
-// 	var sidebar = $('.l-sidebar');
-// 	var body = $('.body');
-// 	$('.c-button--js').on('click',function(){
-// 		// .l-sidebarにis-openクラスを付与
-// 		$('body').toggleClass('is-open');
-// 		$('.l-sidebar').toggleClass('is-open');
-// 		// if(body.hasClass('is-open')) {
-// 		// 	$('.body').removeClass('is-open');
-// 		// 	$('.l-sidebar').removeClass('is-open');
-// 		// }
-// 	})
-// });
 jQuery(function($){
-	// ウインドウサイズがbreakpoint-large（1025）になったら非表示にさせる
-	$('.c-button--js').on('click',function(){
+	// ウインドウサイズの変更がなくても、クリックイベントでクラスの付与
+	$('.c-button--js').on('click', function(){
 		$('body').toggleClass('is-open');
 		$('.l-sidebar').toggleClass('is-open');
-		console.log('Menuがクリックされ、is-openが付与されました！');
 	});
+	// 閉じるボタン以外でもメニューバー以外をクリックすると閉じる処理
+	$(document).on('click', function(e) {
+		// メニューボタンがなく、l-sidebarが
+		if (!$(e.target).closest('.c-button--menu').length && !$(e.target).closest('.l-sidebar').length) {
+			if($('.l-sidebar').hasClass('is-open')){
+				$('body').removeClass('is-open');
+				$('.l-sidebar').removeClass('is-open');
+			}
+		}
+	});
+	// ウインドウが1025以下だった場合is-open削除
 	$(window).resize(function(){
 		let $window = $(this).width();
 		let large = 1025;
-		// if($window < large){
-			// $('.c-button--js').on('click',function(){
-			// 	console.log('サイズが変更されました！');
-			// 	$('body').toggleClass('is-open');
-			// 	$('.l-sidebar').toggleClass('is-open');
-			// });
-		// }
 		if($window > large){
-			console.log('1025より幅大きい');
 			$('body').removeClass('is-open');
-			console.log('bodyのis-openは削除されました');
 			$('.l-sidebar').removeClass('is-open');
-			console.log('l-sidebarのis-openは削除されました');
-		}
-		else{
-			console.log('1025より幅小さい');
-			// $('.body').removeClass('is-open');
-			// $('.l-sidebar').removeClass('is-open');
-			// console.log('is-openは削除されました');
 		}
 	});
 });
